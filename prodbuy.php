5100ddb031c1cd00bec7f1d8427a49e32ac804be
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-09 17:56:50 +0530

    Retrieve and display product ID from query string in prodbuy.php for debugging

--- prodbuy.php
+++ prodbuy.php
@@ -6,6 +6,12 @@ echo "<body>";
 include ("headfile.html"); //include header layout file
 echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
 //display random text
+//retrieve the product id passed from previous page using the GET method and the $_GET superglobal variable
+//applied to the query string u_prod_id
+//store the value in a local variable called $prodid
+$prodid=$_GET['u_prod_id'];
+//display the value of the product id, for debugging purposes
+echo "<p>Selected product Id: ".$prodid;
 
 include("footfile.html"); //include head layout
 echo "</body>";
