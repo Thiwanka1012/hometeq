d3a6200f06666e22473fcdc7ca4bc123b8fe434c
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-29 12:32:39 +0530

    Refactor basket and template files to improve structure and add placeholder content

--- template.php
+++ template.php
@@ -1,10 +1,16 @@
 <?php
 $pagename="template"; //Create and populate a variable called $pagename
+
 echo "<link rel=stylesheet type=text/css href=mystylesheet.css>"; //Call in stylesheet
+
 echo "<title>".$pagename."</title>"; //display name of the page as window title
+
 echo "<body>";
+
 include ("headfile.html"); //include header layout file
+
 echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
+
 //display random text
 echo "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
 labore et dolore magna aliqua. Non consectetur a erat nam at lectus urna. Cras pulvinar mattis nunc sed
@@ -16,6 +22,9 @@ volutpat sed. Nulla malesuada pellentesque elit eget. Varius quam quisque id dia
 pulvinar. Aliquet eget sit amet tellus cras adipiscing enim eu turpis. Vestibulum lectus mauris ultrices
 eros in. Faucibus in ornare quam viverra. Hac habitasse platea dictumst vestibulum rhoncus. Parturient
 montes nascetur ridiculus mus. Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna.";
+
 include("footfile.html"); //include head layout
+
 echo "</body>";
+
 ?>
